5a7fdb05ccabfa26d745b18a81e3ad22d20f70b0
Lukas <lukasduechting@mailbox.org>
2022-05-18 12:35:20 +0200

    amend

--- src/main/java/com/github/mysterix5/IntExercise.java
+++ src/main/java/com/github/mysterix5/IntExercise.java
@@ -1,8 +1,12 @@
 package com.github.mysterix5;
 
 public class IntExercise {
+
+    static boolean check_overly_big_number(int input_int){
+        return input_int > 100;
+    }
     static int add(int a, int b){
-        if(a>100 || b>100){
+        if(check_overly_big_number(a) || check_overly_big_number(b)){
             throw new IllegalArgumentException("Die Zahlen, die addiert werden sollen, müssen <= 100 sein");
         }
         return a+b;
